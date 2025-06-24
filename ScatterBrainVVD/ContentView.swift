a8fd797479f1188627d52881d71ed1e8100d2d35
Fin-Murphy <finnianmurph@gmail.com>
2025-06-24 14:35:11 -0400

    bandaid fix for dark scheme blowing up

--- ScatterBrainVVD/ContentView.swift
+++ ScatterBrainVVD/ContentView.swift
@@ -692,7 +692,7 @@ struct ContentView: View {
             Spacer()
                         
             TabBar(selectedTab: $selectedTab)
-        }
+        }.colorScheme(.light)
     }
 
 /*    ------------------------------------------------
@@ -1108,34 +1108,6 @@ struct ContentView: View {
 
 
 
-
-
-
-
-
-
-
-
-        
-    /*    ------------------------------------------------
-                   ADD ITEM
-     ------------------------------------------------     */
-    
-//    private func addItem() {
-//        withAnimation {
-//            let newItem = Item(context: viewContext)
-//            newItem.timestamp = Date()
-//
-//            do {
-//                try viewContext.save()
-//            } catch {
-//                // Replace this implementation with code to handle the error appropriately.
-//                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-//                let nsError = error as NSError
-//                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-//            }
-//        }
-//    }
 
     /*    ------------------------------------------------
                    DELETE ITEM
