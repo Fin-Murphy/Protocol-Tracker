f31a636da540575b21fd466349a6b6f97adc24f9
Fin-Murphy <finnianmurph@gmail.com>
2025-07-22 20:35:35 -0400

    Protocol view shtuff

--- ScatterBrainVVD/BuilderViews/GoalSetView.swift
+++ ScatterBrainVVD/BuilderViews/GoalSetView.swift
@@ -13,34 +13,7 @@ struct GoalSetView: View {
     @State var displayCompletedHabits: Bool = false
     var body: some View {
       
-        VStack {
-            HStack {
-                Text("Set your daily goal:")
-                
-                TextField("", value: $dailyGoalSet, format: .number)
-                    .frame(maxWidth: 100, alignment: .center)
-            }
-            .foregroundColor(ForeColor)
-            .bckMod()
-            
-            Button {
-                UserDefaults.standard.set(dailyGoalSet, forKey: "dailyGoal")
-            } label: {
-                Text("Save Daily Goal")
-            }
-            .foregroundColor(ForeColor)
-            .bckMod()
-            
-//            Toggle("Move complete habits to bottom of list",isOn: $displayCompletedHabits)
-//                .onAppear{displayCompletedHabits = UserDefaults.standard.bool(forKey: "displayCompletedHabits")}
-//                .onChange(of: displayCompletedHabits) {
-//                    UserDefaults.standard.set(displayCompletedHabits, forKey: "displayCompletedHabits")
-//                }
-//            
-        }
-        .onAppear{dailyGoalSet = UserDefaults.standard.integer(forKey: "dailyGoal")}
-        .foregroundColor(ForeColor)
-        .bckMod()
+// 
         
         ProtocolListView()
   
